4615740aa412e2341bf949c525f1b42a81b2735b
Zach Briggs <briggszj@gmail.com>
2025-11-11 22:13:26 -0500

    [task-1-2] Distinguish between blank and non blank files

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module PolicyOcr
+  def self.call(filename)
+    line_count = 0
+    File.foreach(filename) do
+      line_count += 1
+    end
+
+    "?" if line_count.positive?
+  end
 end
