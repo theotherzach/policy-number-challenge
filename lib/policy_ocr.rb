30f2d1620c448ca2f83c922258bc85563fa88051
Zach Briggs <briggszj@gmail.com>
2025-11-14 13:03:00 -0500

    [task-1-4a] Refactor create_num_blocks

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -1,21 +1,32 @@
 # frozen_string_literal: true
 
+require "pp"
+
 module PolicyOcr
   def self.call(filename)
-    rows = [[]]
-    File.foreach(filename) do |line|
-      if line.strip.empty?
-        unless rows.last.empty?
-          parse_row(rows.last)
-          rows.push([])
-        end
-      else
-        rows.last.push(line)
-      end
+    entry = []
+    File.foreach(filename).with_index do |line, i|
+      entry = [] if (i % 4).zero?
+      entry.push(line.chomp) if i % 4 < 3
+      create_num_blocks(entry) if i % 4 == 3
     end
   end
 
-  def self.parse_row(row)
-    puts (row.first.size / 3).times.map { "?" }.join
+  # ["    _  _     _  _  _  _  _ ",
+  #  "  | _| _||_||_ |_   ||_||_|",
+  #  "  ||_  _|  | _||_|  ||_| _|"]
+  def self.create_num_blocks(entry)
+    num_blocks = []
+    entry.each do |row|
+      row.chars.each_slice(3).with_index do |(*a), i|
+        num_blocks[i] ||= {
+          block: [],
+          resolution: "?"
+        }
+        num_blocks[i][:block].push(a)
+      end
+    end
+    puts num_blocks.map { |e| e.fetch(:resolution) }.join
+    num_blocks
   end
 end
--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -10,4 +10,32 @@ describe PolicyOcr do
   it "loads the sample.txt" do
     expect(fixture("sample").lines.count).to eq(44)
   end
+
+  describe ".create_num_blocks" do
+    context "happy path" do
+      let(:entry) do
+        ["    _  _     _  _  _  _  _ ",
+         "  | _| _||_||_ |_   ||_||_|",
+         "  ||_  _|  | _||_|  ||_| _|"]
+      end
+
+      let(:expected_result) do
+        [
+          { block: [[" ", " ", " "], [" ", " ", "|"], [" ", " ", "|"]], resolution: "?" },
+          { block: [[" ", "_", " "], [" ", "_", "|"], ["|", "_", " "]], resolution: "?" },
+          { block: [[" ", "_", " "], [" ", "_", "|"], [" ", "_", "|"]], resolution: "?" },
+          { block: [[" ", " ", " "], ["|", "_", "|"], [" ", " ", "|"]], resolution: "?" },
+          { block: [[" ", "_", " "], ["|", "_", " "], [" ", "_", "|"]], resolution: "?" },
+          { block: [[" ", "_", " "], ["|", "_", " "], ["|", "_", "|"]], resolution: "?" },
+          { block: [[" ", "_", " "], [" ", " ", "|"], [" ", " ", "|"]], resolution: "?" },
+          { block: [[" ", "_", " "], ["|", "_", "|"], ["|", "_", "|"]], resolution: "?" },
+          { block: [[" ", "_", " "], ["|", "_", "|"], [" ", "_", "|"]], resolution: "?" }
+        ]
+      end
+
+      it "returns 9 num_blocks" do
+        expect(described_class.create_num_blocks(entry)).to eq(expected_result)
+      end
+    end
+  end
 end
